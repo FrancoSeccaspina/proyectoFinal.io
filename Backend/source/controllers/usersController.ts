612886b37f2c709fc8c472b49b44d583b6b8275e
Santino Tassara Franco <santinotassarafrancoar@gmail.com>
2025-04-14 23:52:49 -0300

    UsersController.Login
    se crea la funcion login en el controlador de usuarios y se modifica el users.routers para poder llamar al login

--- Backend/source/controllers/usersController.ts
+++ Backend/source/controllers/usersController.ts
@@ -35,24 +35,24 @@ export class UsuarioController {
     try {
       const reultadosValidacion = validationResult(req);
       const errores = reultadosValidacion.mapped();
-      const { apellido, nombre, imagen, email, contrasenia, fecha_nacimiento} = req.body;
+      const { apellido, nombre, imagen, email, contrasenia, fecha_nacimiento } = req.body;
 
       const usuarioExistente = await Autenticacion.findOne({ where: { email } });
-      if(usuarioExistente){
-          console.log('El email ya existe')
-          res.render('register',{
-              errorEmail:{email:{msg:'Email ya registrado'}},
-              oldData: req.body,
-              errors: errores
-          })
-          return res
+      if (usuarioExistente) {
+        console.log('El email ya existe')
+        res.render('register', {
+          errorEmail: { email: { msg: 'Email ya registrado' } },
+          oldData: req.body,
+          errors: errores
+        })
+        return res
       }
 
-      if(!reultadosValidacion.isEmpty()){
-        res.render('register',{
-            errorEmail:{email:{msg:''}},
-            oldData: req.body,
-            errors: errores
+      if (!reultadosValidacion.isEmpty()) {
+        res.render('register', {
+          errorEmail: { email: { msg: '' } },
+          oldData: req.body,
+          errors: errores
         })
         return res
       }
@@ -67,20 +67,20 @@ export class UsuarioController {
         },
         { transaction }
       );
-  
+
       // TODO: agregar el numero de vueltas en una variable de entorno
-      const hashedPassword = bcrypt.hashSync(contrasenia, 10); 
+      const hashedPassword = bcrypt.hashSync(contrasenia, 10);
       await Autenticacion.create(
         {
-          email : email,
+          email: email,
           contrasenia: hashedPassword,
           id_usuario: nuevoUsuario.id,
         },
         { transaction }
       );
-  
+
       await transaction?.commit();
-      
+
       res.redirect('/login')
       return res
 
@@ -94,5 +94,36 @@ export class UsuarioController {
       });
     }
   }
+  async login(req: Request, res: Response): Promise<Response> {
+    try {
+      const { email, contrasenia } = req.body;
+      const usuarioExistente = await Autenticacion.findOne({ where: { email } });
+      if (!usuarioExistente) {
+        return res.status(401).json({
+          success: false,
+          message: "Datos incorrectos",
+        });
+      }
+      const verificarContrasenia = bcrypt.compareSync(contrasenia, usuarioExistente.contrasenia);
+      if (!verificarContrasenia) {
+        return res.status(401).json({
+          success: false,
+          message: "Contraseña incorrecta",
+        });
+      }
+      return res.status(200).json({
+        success: true,
+        message: "Usuario logueado correctamente",
+        usuarioExistente,
+      });
+    } catch (error) {
+      console.error("Error en login:", (error as Error).message);
+      return res.status(500).json({
+        success: false,
+        message: "Error",
+      });
+    }
+  }
+
 }
 export default new UsuarioController();
--- Backend/source/routes/users.routes.ts
+++ Backend/source/routes/users.routes.ts
@@ -4,12 +4,13 @@ const registerValidator = require('../validations/register')
 
 const route = express.Router();
 
-route.get('/users/form', (req, res) => { 
+route.get('/users/form', (req, res) => {
     res.render('register');
 });
 
 route.get('/users/show/:id', (req, res) => { usersController.show(req, res) });
 route.post('/users/save', registerValidator, (req, res) => { usersController.save(req, res) });
+route.post('/users/login', (req, res) => { usersController.login(req, res) });
 // route.post('/register/save', (req, res) => { usersController.register(req, res) });
 
 // route.get('/register', (req, res) => { usersController.register(req, res) });
