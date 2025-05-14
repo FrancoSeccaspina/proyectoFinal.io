892f94e64099125d33192543db4295e12a3c7543
gjimenez <gustavo.jimenez.crespo@gmail.com>
2025-05-14 11:41:51 -0300

    comentar rutas carrito

--- Backend/source/routes/carrito.routes.ts
+++ Backend/source/routes/carrito.routes.ts
@@ -4,9 +4,9 @@ import carritoController from "../controllers/carritoController";
 const router = Router();
 
 // Rutas del carrito
-router.post("/carrito/agregar", (req, res) => carritoController.agregarProducto(req, res));
-router.delete("/carrito/eliminar/:id", (req, res) => carritoController.eliminarProducto(req, res));
-router.get("/carrito", (req, res) => carritoController.verCarrito(req, res));
-router.post("/carrito/finalizar", (req, res) => carritoController.finalizarCompra(req, res));
+// router.post("/carrito/agregar", (req, res) => carritoController.agregarProducto(req, res));
+// router.delete("/carrito/eliminar/:id", (req, res) => carritoController.eliminarProducto(req, res));
+// router.get("/carrito", (req, res) => carritoController.verCarrito(req, res));
+// router.post("/carrito/finalizar", (req, res) => carritoController.finalizarCompra(req, res));
 
 export default router;
