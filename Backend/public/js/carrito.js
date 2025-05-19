de7bc77e658d1c25a3c33d3bf61e68cc65729c1f
gjimenez <gustavo.jimenez.crespo@gmail.com>
2025-05-19 20:10:49 -0300

    Modal de confirmacion

--- Backend/public/js/carrito.js
+++ Backend/public/js/carrito.js
@@ -1,24 +1,3 @@
-// ventana modal
-document.querySelector('.modal-overlay').style.display = 'none';
-document.querySelectorAll('.eliminar-btn').forEach(btn => {
-    btn.addEventListener('click', function (e) {
-        e.preventDefault();
-        const form = this.closest('form');
-        document.querySelector('.modal-overlay').style.display = 'grid';
-
-        // Cancelar
-        document.querySelector('.btn-cancel').onclick = function () {
-            document.querySelector('.modal-overlay').style.display = 'none';
-        };
-
-        // eliminar
-        document.querySelector('.btn-confirm').onclick = function () {
-            document.querySelector('.modal-overlay').style.display = 'none';
-            form.submit();
-        };
-    });
-});
-
 // Guardar la posición del scroll antes de recargar
 // Restaurar la posición del scroll al cargar la página
 window.addEventListener('beforeunload', function () {
