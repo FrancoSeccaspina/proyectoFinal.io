e672c4b63e767a65c7f72939471fc66a8cd905be
gjimenez <gustavo.jimenez.crespo@gmail.com>
2025-05-19 19:38:28 -0300

    Modal de confirmacion

--- Backend/public/js/carrito.js
+++ Backend/public/js/carrito.js
@@ -1,12 +1,32 @@
-  // Guardar la posición del scroll antes de recargar
-  // Restaurar la posición del scroll al cargar la página
+// ventana modal
+document.querySelector('.modal-overlay').style.display = 'none';
+document.querySelectorAll('.eliminar-btn').forEach(btn => {
+    btn.addEventListener('click', function (e) {
+        e.preventDefault();
+        const form = this.closest('form');
+        document.querySelector('.modal-overlay').style.display = 'grid';
 
-  window.addEventListener('beforeunload', function () {
+        // Cancelar
+        document.querySelector('.btn-cancel').onclick = function () {
+            document.querySelector('.modal-overlay').style.display = 'none';
+        };
+
+        // eliminar
+        document.querySelector('.btn-confirm').onclick = function () {
+            document.querySelector('.modal-overlay').style.display = 'none';
+            form.submit();
+        };
+    });
+});
+
+// Guardar la posición del scroll antes de recargar
+// Restaurar la posición del scroll al cargar la página
+window.addEventListener('beforeunload', function () {
     sessionStorage.setItem('scrollPos', window.scrollY);
-  });
+});
 
-  window.addEventListener('load', function () {
+window.addEventListener('load', function () {
     const scrollPos = sessionStorage.getItem('scrollPos');
     if (scrollPos) window.scrollTo(0, parseInt(scrollPos, 10));
-  });
+});
 
