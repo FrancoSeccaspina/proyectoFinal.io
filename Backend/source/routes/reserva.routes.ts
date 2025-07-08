b7fe4cb5089a17953241e68cf180d70e3739a184
gjimenez <gustavo.jimenez.crespo@gmail.com>
2025-07-08 17:02:52 -0300

    Agregar navegador de interfaces cliente admin

--- Backend/source/routes/reserva.routes.ts
+++ Backend/source/routes/reserva.routes.ts
@@ -6,10 +6,10 @@ import reservaController from '../controllers/reservaController'
 const route = Router();
 
 // Rutas del reservas
-route.get("/reserva/mostrar/ultimaReserva", verificarTokenPorRol([Roles.CLIENTE]), (req: Request, res: Response) => { reservaController.mostrarUltimaReserva(req, res) });
-route.get("/reserva/mostrar/reserva/:id", verificarTokenPorRol([Roles.CLIENTE]), (req: Request, res: Response) => { reservaController.mostrarReservaPorId(req, res) });
-route.get("/reserva/mostrar/reservas", verificarTokenPorRol([Roles.CLIENTE]), (req: Request, res: Response) => { reservaController.mostrarReservas(req, res) });
-route.post("/reserva/agregar", verificarTokenPorRol([Roles.CLIENTE]), (req: Request, res: Response) => { reservaController.reservarCompra(req, res); });
-route.patch("/reserva/eliminar/:id", verificarTokenPorRol([Roles.CLIENTE]), (req: Request, res: Response) => { reservaController.cancelarReservaPorId(req, res) });
+route.get("/reserva/mostrar/ultimaReserva", verificarTokenPorRol([Roles.CLIENTE, Roles.ADMIN]), (req: Request, res: Response) => { reservaController.mostrarUltimaReserva(req, res) });
+route.get("/reserva/mostrar/reserva/:id", verificarTokenPorRol([Roles.CLIENTE, Roles.ADMIN]), (req: Request, res: Response) => { reservaController.mostrarReservaPorId(req, res) });
+route.get("/reserva/mostrar/reservas", verificarTokenPorRol([Roles.CLIENTE, Roles.ADMIN]), (req: Request, res: Response) => { reservaController.mostrarReservas(req, res) });
+route.post("/reserva/agregar", verificarTokenPorRol([Roles.CLIENTE, Roles.ADMIN]), (req: Request, res: Response) => { reservaController.reservarCompra(req, res); });
+route.patch("/reserva/eliminar/:id", verificarTokenPorRol([Roles.CLIENTE, Roles.ADMIN]), (req: Request, res: Response) => { reservaController.cancelarReservaPorId(req, res) });
 
 export default route;
--- frontend/src/component/Sidebar.jsx
+++ frontend/src/component/Sidebar.jsx
@@ -26,6 +26,28 @@ function Sidebar(){
                     <a href="http://localhost:3032/users/logout">Volver</a>
                 </div>
             </div>
+            
+            <div class="flotante">
+            <nav aria-label="Breadcrumb">
+                <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                    <a href="http://localhost:3032/" class="breadcrumb-link">Cliente</a>
+                </li>
+
+                <li class="breadcrumb-separator">
+                    <svg class="breadcrumb-icon" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m9 20.247 6-16.5" />
+                    </svg>
+                </li>
+
+                <li class="breadcrumb-item">
+                    <a href="http://localhost:3000" class="breadcrumb-link">Admin</a>
+                </li>
+                </ol>
+            </nav>
+
+            </div>
         </React.Fragment>
     )
 }
