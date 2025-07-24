e7589c3cb720ed4d918b4f9ff9069421fa1ee1e6
gjimenez <gustavo.jimenez.crespo@gmail.com>
2025-07-24 12:24:44 -0300

    Agregar contenedor de foto de perfil

--- Backend/source/database/models/usuario.ts
+++ Backend/source/database/models/usuario.ts
@@ -68,7 +68,7 @@ const initUsuarioModel = (sequelize: Sequelize) => {
       imagen: {
         type: DataTypes.STRING(255),
         allowNull: true,
-        defaultValue: 'perfil.jpg'  // <-- valor por defecto
+        defaultValue: 'perfil.jpg'
       },
       id_membresia: {
         type: DataTypes.INTEGER,
