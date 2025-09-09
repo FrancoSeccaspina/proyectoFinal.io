e379c1b039ebe83780e88c9ffe7586f52568fbaf
gjimenez <gustavo.jimenez.crespo@gmail.com>
2025-09-08 22:44:14 -0300

    Refactor .envs

---
+++ build_frontend.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+FRONTEND_DIR="$(pwd)/frontend"
+FLASK_STATIC_DIR="$(pwd)/flask/app/static"
+BUILD_DIR="$FRONTEND_DIR/build"
+
+cd "$FRONTEND_DIR"
+npm install
+npm run build
+
+if [ -d "$FLASK_STATIC_DIR" ]; then
+    rm -rf "$FLASK_STATIC_DIR"
+fi
+
+mv "$BUILD_DIR" "$FLASK_STATIC_DIR"
+
+echo "Build frontend completado y movido a flask/static."
